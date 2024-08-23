cbc3b46248c70769591c9c4250e06efa98271600
Avinash045 <164344545+Avinash045@users.noreply.github.com>
2024-08-23 08:06:49 +0000

    container cinfig done

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all js code as newer version.
+
+// alert(3 + 3)  // we are using node js not browser.
+
+console.log(3 + 3);  //  code readability should be high
+console.log("Avinash");
+
+let name = "Avinash";     // string => ""
+let age = 22;             // number => 2 to power 
+let isLoggedIn = false;   // boolean => true or false.
+let state = null;         // null => standalone value.
+let country;              // undefined =>
+                          // symbol => unique
+
+console.table([name, age, isLoggedIn, state, country]);
+
+console.log(typeof name)   // it will show the datattype of variable.
+console.log(typeof null);  // it will give object.
+console.log(typeof undefined);   // datattype of ndefined is always undefined.
+
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,23 @@
 console.log("Avinash");
+const accountId = 12345
+let accountEmail = "avinash@gmail.com"
+var accountPassword = "431511"
+accountCity = "Deulgaon"   // dont use this type of declaration. use always 'let' instead of 'var' due to  issue in block scope
+//                            and functional scope.
+let accountState;         // it will give output as undefined.
+// accountId = 2 // Not allowed to change contstant varaiable.
+
+accountEmail = "harihar@hotmail.com"
+accountPassword = 431402
+accountCity = "Mahagaon"
+
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountEmail);
+
+// or you can use following instead of seperate console.
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
