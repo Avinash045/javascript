fc80bf5e672d63ee1af7353c41e35aea4c8f953a
Avinash045 <164344545+Avinash045@users.noreply.github.com>
2024-09-03 07:43:30 +0000

    scope done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -23,4 +23,47 @@ if(true){
     const b = 20  
     var c = 30
 }
-console.log(c);   // it will give value of local variable at global space. So don't use var.
+// console.log(c);   // it will give value of local variable at global space. So don't use var.
+
+// ************************************************************************************************
+
+function hari(){
+    const name = "Harihar Bhalerao"
+    function avii(){
+         const myName = "Avinash Bhalerao"
+//         console.log(name);                           // can be accessible. global variable can access at local space. 
+//         console.log(myName);
+     }
+    avii()
+//     // console.log(myName);                   // cannot access local varibale at global space due its scope.
+//     console.log(name);
+ }
+ hari()
+
+
+if(true){
+    const username = "Avinash"
+    if(username === "Avinash"){
+        const website = "Instagram"
+        console.log(username + website);                 
+    }
+    // console.log(website);                   here also we cannot access the local variable at global space due to its scope. 
+    console.log(username);
+}
+
+// ********************************* mini hoisting **************************
+
+
+// console.log(addition(7));                 we can call a normal function before its declarartion.
+function addition(num){
+    return num + 2;
+}
+console.log(addition(5));
+
+
+
+// console.log(anotherAdd(8));                but we could not call a function stored in a varibale. 
+const anotherAdd = function(num){
+    return num + 7;
+}
+console.log(anotherAdd(8));
